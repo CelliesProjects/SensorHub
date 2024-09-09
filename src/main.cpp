9adce124b1011ca1f76088d112a0c0fe4c179018
Cellie <CelliesProjects@users.noreply.github.com>
2024-09-09 16:11:02 +0200

    Added the lolin s2 mini as compile target

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 
 #include "wifiSecrets.h"
 
+#include "storageStruct.h"
+
 IPAddress STATIC_IP(192, 168, 0, 20);
 IPAddress SUBNET(255, 255, 255, 0);
 IPAddress GATEWAY(192, 168, 0, 1);
@@ -25,6 +27,9 @@ static S8_UART *sensor_S8;
 static PsychicHttpServer server;
 static PsychicWebSocketHandler websocketHandler;
 
+static storageStruct lastResults{-200 /* temp */, 0 /* co2 */, 0 /* humidity */};
+static storageStruct history[90];
+
 void setup()
 {
     Serial.begin(115200);
@@ -35,30 +40,23 @@ void setup()
 
     delay(5000);
 
-    Serial.println("SHT31 test");
-    Wire.begin(1, 2);
-    if (!sht31.begin(0x44))
+    Wire.begin(SHT31_SDA, SHT31_SCL);
+    if (!sht31.begin(SHT31_DEFAULT_ADDR))
     {
-        Serial.println("Couldn't find SHT31");
+        log_e("Couldn't find SHT31 at address %u", SHT31_DEFAULT_ADDR);
         while (1)
             delay(1);
     }
 
-    Serial.print("Heater Enabled State: ");
-    if (sht31.isHeaterEnabled())
-        Serial.println("ENABLED");
-    else
-        Serial.println("DISABLED");
-
     log_i("temp %.1f", sht31.readTemperature());
 
-    Serial1.setPins(21, 20);
+    Serial1.setPins(SENSEAIR_RXD, SENSEAIR_TXD);
     Serial1.begin(S8_BAUDRATE);
     sensor_S8 = new S8_UART(Serial1);
 
     if (!sensor_S8)
     {
-        log_i("FATAL ERROR! Could not initialize CO2 sensor.");
+        log_e("FATAL ERROR! Could not initialize CO2 sensor.");
         while (1)
             delay(100);
     }
@@ -67,7 +65,7 @@ void setup()
 
     /* try to set a static IP */
     if (!WiFi.config(STATIC_IP, GATEWAY, SUBNET, DNS_SERVER))
-        Serial.println("Setting static IP failed");
+        log_e("Setting static IP failed");
 
     // connect to wifi
     WiFi.mode(WIFI_MODE_AP);
@@ -77,9 +75,9 @@ void setup()
     while (!WiFi.isConnected())
         delay(10);
 
-    Serial.printf("Connected to %s\n", SSID);
-    Serial.printf("IP %s\n", WiFi.localIP().toString().c_str());
-    Serial.printf("Sensor websocket started at http://%s%s\n", WiFi.localIP().toString().c_str(), SENSORS_WS_URL);
+    log_i("Connected to %s", SSID);
+    log_i("IP %s", WiFi.localIP().toString().c_str());
+    log_i("Sensor websocket started at http://%s%s", WiFi.localIP().toString().c_str(), SENSORS_WS_URL);
 
     // setup the webserver
 
@@ -88,75 +86,64 @@ void setup()
     server.listen(80);
 
     websocketHandler.onOpen([](PsychicWebSocketClient *client)
-    {
-        Serial.printf("[socket] connection #%u connected from %s\n", client->socket(), client->remoteIP().toString());
-        client->sendMessage("Hello!"); 
-    });
+                            {
+        log_v("[socket] connection #%u connected from %s", client->socket(), client->remoteIP().toString());
+        char buff[16];
+        snprintf(buff, sizeof(buff), "H:%i", lastResults.humidity);
+        client->sendMessage(buff);
+        snprintf(buff, sizeof(buff), "T:%.1f", lastResults.temp);
+        client->sendMessage(buff);
+        snprintf(buff, sizeof(buff), "C:%i", lastResults.co2);
+        client->sendMessage(buff); });
 
     websocketHandler.onClose([](PsychicWebSocketClient *client)
-    { 
-        Serial.printf("[socket] connection #%u closed from %s\n", client->socket(), client->remoteIP().toString()); 
-    });
+                             { Serial.printf("[socket] connection #%u closed from %s\n", client->socket(), client->remoteIP().toString()); });
 
     server.on(SENSORS_WS_URL, &websocketHandler);
 
     server.on("/ip", [](PsychicRequest *request)
-    {
+              {
         String output = "Your IP is: " + request->client()->remoteIP().toString();
-        return request->reply(output.c_str()); 
-    });
+        return request->reply(output.c_str()); });
 
     server.on("/hello", HTTP_GET, [](PsychicRequest *request)
-    {
+              {
         String hello = "Hello world!";
-        return request->reply(200, "text/html", hello.c_str()); 
-    });
+        return request->reply(200, "text/html", hello.c_str()); });
 
     server.onNotFound([](PsychicRequest *request)
-    { 
-        return request->reply(404, "text/html", "No page here. Use websocket to connect to /ws."); 
-    });
+                      { return request->reply(404, "text/html", "No page here. Use websocket to connect to /ws."); });
 }
-
-bool enableHeater = false;
-uint8_t loopCnt = 0;
 
 void loop()
 {
-    static int32_t lastCo2 = 0;
-    static float lastTemp = -200;
-    static int32_t lastHumidity = 0;
-
     int32_t co2Level = sensor_S8->get_co2();
     float temp = sht31.readTemperature();
-    int32_t humidity = sht31.readHumidity(); // save the int part to reduce noise
+    int32_t humidity = sht31.readHumidity(); // only use the integer part to reduce noise
 
     temp = static_cast<float>(static_cast<int>(temp * 10.)) / 10.; // round off to 1 decimal place to reduce noise
 
-    char responseBuffer[16];
+    static char responseBuffer[16];
 
-    if (co2Level && co2Level != lastCo2)
+    if (co2Level && co2Level != lastResults.co2)
     {
         snprintf(responseBuffer, sizeof(responseBuffer), "C:%i", co2Level);
         websocketHandler.sendAll(responseBuffer);
-        Serial.printf("Co2 level updated to %i\n", co2Level);
-        lastCo2 = co2Level;
+        lastResults.co2 = co2Level;
     }
 
-    if (!isnan(temp) && temp != lastTemp)
+    if (!isnan(temp) && temp != lastResults.temp)
     {
         snprintf(responseBuffer, sizeof(responseBuffer), "T:%.1f", temp);
         websocketHandler.sendAll(responseBuffer);
-        Serial.printf("temp updated to %.1f\n", temp);
-        lastTemp = temp;
+        lastResults.temp = temp;
     }
 
-    if (!isnan(humidity) && humidity != lastHumidity)
+    if (!isnan(humidity) && humidity != lastResults.humidity)
     {
         snprintf(responseBuffer, sizeof(responseBuffer), "H:%i", humidity);
         websocketHandler.sendAll(responseBuffer);
-        Serial.printf("humidity updated to %i\n", humidity);
-        lastHumidity = humidity;
+        lastResults.humidity = humidity;
     }
 
     delay(200);
